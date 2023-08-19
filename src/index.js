5c8824590912e263331345bfeb631bc5c0056ec2
Nadin21 <nadin21dir@gmail.com>
2023-08-19 15:05:40 +0300

    feat: added function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array_str = [];
+  bracketsConfig.forEach(element => {
+    array_str.push(element.join(''));
+  })
+  let result_str = str;
+  let length_array = array_str.length;
+  do {
+    let i = 0;
+    array_str.forEach(element => {
+      if (~result_str.indexOf(element)) result_str = result_str.replaceAll(element, '');
+      else i++;
+      if (i == length_array) break;
+    })
+
+  } while (result_str);
+
+
 }
